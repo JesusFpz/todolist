f6046bbf2007b1e6a50e349b3d3dcd292995d201
Jesus F Plaz <jfplaz@gmail.com>
2024-12-22 09:22:54 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [listaTareas, setListaTareas] = useState([]);
+    const [tarea, setTarea] = useState("");
+
+    //  agregar tareas
+    const agregarTarea = (e) => {
+        if (e.key === "Enter" && tarea !== "") {
+            setListaTareas([...listaTareas, tarea]);
+            setTarea(""); 
+        }
+    };
+
+    //eliminar una tarea
+    const eliminarTarea = (index) => {
+        const nuevasTareas = listaTareas.filter((_, i) => i !== index);
+        setListaTareas(nuevasTareas);
+    };
+
+    return (
+        <div className="container mt-5">
+            <h1>Mi lista de tareas</h1>
+            <input
+                type="text"
+                onChange={(e) => setTarea(e.target.value)}
+                value={tarea}
+                placeholder="Escribe una tarea"
+                onKeyDown={agregarTarea} 
+            />
+            <ul className="list-group mt-3">
+                {listaTareas.map((tarea, index) => (
+                    <li key={index} className="list-group-item d-flex justify-content-between">
+                        {tarea}
+                        <button 
+                            className="btn btn-danger btn-sm" 
+                            onClick={() => eliminarTarea(index)}
+                        >
+                            X
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Home;
